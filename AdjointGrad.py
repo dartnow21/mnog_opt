0e9f0daa943ec9cb9dda12545e92d95f2745c4cd
dartnow21 <83269930+dartnow21@users.noreply.github.com>
2022-04-07 18:21:31 +0300

    Add files via upload

--- AdjointGrad.py
+++ AdjointGrad.py
@@ -1,13 +1,14 @@
-import pandas as pd
-from sympy import *
+from mnog_opt.BrentMet_var2_help import *
+import numpy as np
 from re import findall
 from re import sub
-import numpy as np
-from mnog_opt.BrentMet_var2_help import *
+import pandas as pd
+from sympy import *
 
 
 class AdjointGrad:
     def find(self, z, x, e, flag1, flag2, extr):
+
         """
         Поиск экстремума функции многих переменных при помощи алгоритма Ньютона-сопряженного градиента
         Parameters
@@ -28,152 +29,80 @@ class AdjointGrad:
             Возвращает точку максимума или минимума, значение функции в точке минимума и датасет с промежуточными
              вычислениями
         """
+
+        df = pd.DataFrame(columns=['Номер итерации', 'Полученные значения'])
+        c=0
         func = sympify(z)
         l = Symbol('l')
         lst_xi = np.sort(list(set(findall(r'[x]\d', z))))
 
-        df = pd.DataFrame(columns=['Номер итерации', 'Полученные значения'])
-
-        det_func = []
+        grad = []
         for i in range(len(lst_xi)):
-            det_func.append(func.diff(lst_xi[i]))
-
+            grad.append(func.diff(lst_xi[i]))
+        x = np.array(x)
         j = 0
-        c = 0
-        while j < 10:
+        while True:
             i = 0
-            podst = []
-            calc_det = []
+            su = dict(zip(lst_xi, x))
+            calc_grad = []
             for k in range(len(lst_xi)):
-                podst.append((lst_xi[k], x[k][0]))
-            for k in range(len(lst_xi)):
-                calc_det.append([det_func[k].subs(podst)])
-            S = np.dot(-1, calc_det)
+                calc_grad.append(grad[k].subs(su))
+            calc_grad = np.array(calc_grad)
+            S = -1*calc_grad
 
-            while i < 10:
+            while i+1<len(lst_xi):
 
-                lam = x + np.dot(l, S)
-                podst_f_l = []
-                for k in range(len(lam)):
-                    podst_f_l.append((lst_xi[k], lam[k][0]))
-                f_l = str(func.subs(podst_f_l))
+                lam = x + l*S
+                su1 = dict(zip(lst_xi, lam))
+                f_l = str(func.subs(su1))
                 f_l = sub(r'l', r'x', f_l)
                 lam = BrentMet()
-                l_min = lam.find(f_l, -5, 5, e, 100)
-                xk = x + np.dot(l_min, S)
+                l_min = lam.find(f_l, -100, 100, e*10, 100)
+                xk = x + l_min*S
 
-                sum_det = 0
-                sum_det1 = 0
-                for k in range(len(calc_det)):
-                    sum_det += (calc_det[k][0]) ** 2
-                    sum_det1 += (xk[k][0]) ** 2
-                Skj = np.dot(-1, xk) + (sum_det1 / sum_det) * S
 
-                sum_Skj = 0
-                for k in range(len(Skj)):
-                    sum_Skj += (Skj[k][0]) ** 2
-
-                podst_after = []
+                su_xk = dict(zip(lst_xi, xk))
+                calc_grad_xk = []
                 for k in range(len(lst_xi)):
-                    podst_after.append((lst_xi[k], xk[k][0]))
+                    calc_grad_xk.append(grad[k].subs(su_xk))
+                calc_grad_xk = np.array(calc_grad_xk)
 
-                x_ = x
-                x = xk
-                x_beauty = []
-                for k in range(len(x)):
-                    x_beauty.append(float(x[k][0]))
+                sum_grad = (calc_grad**2).sum()
+                sum_grad_k = (calc_grad_xk**2).sum()
+                Skj = -1*calc_grad_xk + (sum_grad_k/sum_grad) * S
+
+                met_Skj = (Skj**2).sum()**(1/2)
+                su_after = su_xk
+
+                x_beauty = xk
                 if flag1 == 1:
                     print(f'Итерация №{i}, точка {tuple(x_beauty)}')
                 if flag2 == 1:
                     df.loc[i] = [i, tuple(x_beauty)]
 
+
+                x_met = ((xk - x)**2).sum()**(1/2)
                 # Написать датасеты использовать x_beauty
                 if extr == 0:
-                    if (sum_Skj ** (1 / 2) < e) or (abs(sum(xk - x_)[0] ** (1 / 2)) < e):
+
+                    if (met_Skj < e) or (x_met < e):
                         print(f'Минимум в точке {tuple(x_beauty)}')
-                        print(f'Значение функции в точке минимума {float(func.subs(podst_after))}')
+                        print(f'Значение функции в точке минимума {float(func.subs(su_after))}')
                         c = 1
                         break
                 else:
-                    if (sum_Skj ** (1 / 2) < e) or (abs(sum(xk - x_)[0] ** (1 / 2)) < e):
+                    if (met_Skj < e) or (x_met < e):
                         print(f'Максимум в точке {tuple(x_beauty)}')
-                        print(f'Значение функции в точке максимума {float(func.subs(podst_after))}')
+                        print(f'Значение функции в точке максимума {float(func.subs(su_after))}')
                         c = 1
                         break
                 S = Skj
                 i += 1
             if c == 1:
                 break
+            x = xk
             j += 1
+        return x_beauty
 
-        if flag2 == 1:
-            print(df)
-            df.to_csv('FastGradDescent_results.csv', index=False)
-            print('Датасет сохранен в папку проекта в формате csv')
-
-
-# while True:
-#     print("Какой экстремум вы хотите найти? 1 - максимум \ 0 - минимум")
-#     q = int(input())
-#     if q == 1:
-#         extr = 1
-#         break
-#     elif q == 0:
-#         extr = 0
-#         break
-#     else:
-#         print("Такой команды нет. Введите снова")
-# print("Введите функцию f(x1, x2, ... , xn). Например: x1**2 + x1*x2**3 + x3**2")
-# f = input()
-# if extr == 1:
-#     f = "- (" + f + ")"
-#     print(f)
-# count_param = np.sort(list(set(re.findall(r'[x]\d', f))))
-# print("Введите шаг. Например: 0.01")
-# l = float(input())
-# print("Введите начальную точку, из которой начинаем спуск.")
-# lst_xi = []
-# for i in range(len(count_param)):
-#     print(f'Введите {count_param[i]}. Например: 1')
-#     xi = [float(input())]
-#     lst_xi.append(xi)
-#
-# while True:
-#     print("Хотите ввести точность оптимизации? 1 - да / 0 - нет")
-#     q = int(input())
-#     if q == 1:
-#         print("Введите точность оптимизации. Например: 0.001")
-#         e = float(input())
-#         break
-#     elif q == 0:
-#         e = 0.0001
-#         break
-#     else:
-#         print("Такой команды нет. Введите снова")
-#
-# while True:
-#     print("Хотите ввести промежуточные результаты? 1 - да / 0 - нет")
-#     q = int(input())
-#     if q == 1:
-#         flag1 = 1
-#         break
-#     elif q == 0:
-#         flag1 = 0
-#         break
-#     else:
-#         print("Такой команды нет. Введите снова")
-#
-# while True:
-#     print("Хотите записать промежуточные результаты в датасет? 1 - да / 0 - нет")
-#     q = int(input())
-#     if q == 1:
-#         flag2 = 1
-#         break
-#     elif q == 0:
-#         flag2 = 0
-#         break
-#     else:
-#         print("Такой команды нет. Введите снова")
-#
-# function = AdjointGrad()
-# function.find(f, lst_xi, e, flag1, flag2, extr)
+#function = AdjointGrad()
+#function.find('3.125*(-0.2*x1 - 0.6*x2 - x3 + 0.6)**2 + 4.5*(-0.166666666666667*x1 - 0.333333333333333*x2 - x3 + 0.666666666666667)**2 + 6.125*(-0.142857142857143*x1 - 0.571428571428571*x2 - x3 + 0.857142857142857)**2 + 8.0*(-0.125*x1 - 0.125*x2 - x3 + 0.625)**2', [-3,1,1], 0.0001, 0, 0, 0)
